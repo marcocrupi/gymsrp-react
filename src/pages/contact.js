afb1e4e21385fb76ac63cd4a64f71b67556c7445
Marco Crupi <marcocrupi@hotmail.it>
2022-08-27 14:35:46 +0200

    update style version 1.0

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -21,27 +21,37 @@ function Contact() {
           <p>I was born in Varese in 1988. </p>
           <p>
             Junior Front-End Developer with a background in professional
-            photography (2015-2022), SEO and digital marketing. I worked 6 years
-            as a Global Brand Ambassador for Panasonic, I collaborated with
-            numerous other companies in the photographic sector, such as: Epson,
-            Nokia, Carl Zeiss, Samsung, Crumpler, Energizer and Manfrotto.
+            photography (2015-2022), SEO and digital marketing.
           </p>
         </div>
-        <div>
-          <a href="mailto:marcocrupi@hotmail.it">
-            <button className="contact__email btn btn-warning btn-lg shadow-none">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-envelope-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
-              </svg>
-            </button>
-          </a>
+        <div className="contact__buttons">
+          <div>
+            <a href="mailto:marcocrupi@hotmail.it">
+              <button className="contact__email btn btn-warning btn-lg shadow-none">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-envelope-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
+                </svg>
+              </button>
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/crupimarco/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="contact__email btn btn-danger btn-lg shadow-none">
+                <ion-icon name="logo-linkedin"></ion-icon>
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
