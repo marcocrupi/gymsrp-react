8a415dff6f83d25bb01952da33f022950985ef2a
Marco Crupi <marcocrupi@hotmail.it>
2022-08-28 09:50:03 +0200

    fix active in bottom navbar

--- src/components/bottomnav.js
+++ src/components/bottomnav.js
@@ -1,71 +1,82 @@
 import React from "react";
 import "../CSS/bottomnav.css";
-import { NavLink } from "react-router-dom";
+import {
+  NavLink,
+  useMatch,
+  useResolvedPath,
+} from "react-router-dom";
 
-function Bottomnav() {
-  const list = document.querySelectorAll(".bottomnav__list");
-  function activeLink() {
-    list.forEach((item) => item.classList.remove("active"));
-    this.classList.add("active");
-  }
-  list.forEach((item) => item.addEventListener("click", activeLink));
+function Bottomnav({ to, ...props }) {
+  //   const list = document.querySelectorAll(".bottomnav__list");
+  //   function activeLink() {
+  //     list.forEach((item) => item.classList.remove("active"));
+  //     this.classList.add("active");
+  //   }
+  //   list.forEach((item) => item.addEventListener("click", activeLink));
 
   return (
     <div className="bottomnav__container">
       <div className="bottomnav__navigation">
         <ul>
-          <li className="bottomnav__list active">
-            <NavLink to="/gymsrp-react" className="bottomnav__link">
-              <span className="bottomnav__icon">
-                <ion-icon name="hourglass-outline"></ion-icon>
-              </span>
-              <span className="bottomnav__text">Timer</span>
-            </NavLink>
-          </li>
-          <li className="bottomnav__list">
-            <NavLink to="/gymsrp-react/set" className="bottomnav__link">
-              <span className="bottomnav__icon">
-                <ion-icon name="add-circle-outline"></ion-icon>
-              </span>
-              <span className="bottomnav__text">Set</span>
-            </NavLink>
-          </li>
-          <li className="bottomnav__list">
-            <NavLink to="/gymsrp-react/percentage" className="bottomnav__link">
-              <span className="bottomnav__icon">
-                <ion-icon name="calculator-outline"></ion-icon>
-              </span>
-              <span className="bottomnav__text">Percentage</span>
-            </NavLink>
-          </li>
-          <li className="bottomnav__list">
-            <NavLink to="/gymsrp-react/rm" className="bottomnav__link">
-              <span className="bottomnav__icon">
-                <ion-icon name="barbell-outline"></ion-icon>
-              </span>
-              <span className="bottomnav__text">1RM</span>
-            </NavLink>
-          </li>
-          <li className="bottomnav__list">
-            <NavLink to="/gymsrp-react/bmi" className="bottomnav__link">
-              <span className="bottomnav__icon">
-                <ion-icon name="accessibility-outline"></ion-icon>
-              </span>
-              <span className="bottomnav__text">BMI</span>
-            </NavLink>
-          </li>
-          <li className="bottomnav__list">
-            <NavLink to="/gymsrp-react/contact" className="bottomnav__link">
-              <span className="bottomnav__icon">
-                <ion-icon name="mail-outline"></ion-icon>
-              </span>
-              <span className="bottomnav__text">Contact</span>
-            </NavLink>
-          </li>
+          <CustomLink to="/gymsrp-react" className="bottomnav__link">
+            <span className="bottomnav__icon">
+              <ion-icon name="hourglass-outline"></ion-icon>
+            </span>
+            <span className="bottomnav__text">Timer</span>
+          </CustomLink>
+
+          <CustomLink to="/gymsrp-react/set" className="bottomnav__link">
+            <span className="bottomnav__icon">
+              <ion-icon name="add-circle-outline"></ion-icon>
+            </span>
+            <span className="bottomnav__text">Set</span>
+          </CustomLink>
+
+          <CustomLink to="/gymsrp-react/percentage" className="bottomnav__link">
+            <span className="bottomnav__icon">
+              <ion-icon name="calculator-outline"></ion-icon>
+            </span>
+            <span className="bottomnav__text">Percentage</span>
+          </CustomLink>
+
+          <CustomLink to="/gymsrp-react/rm" className="bottomnav__link">
+            <span className="bottomnav__icon">
+              <ion-icon name="barbell-outline"></ion-icon>
+            </span>
+            <span className="bottomnav__text">1RM</span>
+          </CustomLink>
+
+          <CustomLink to="/gymsrp-react/bmi" className="bottomnav__link">
+            <span className="bottomnav__icon">
+              <ion-icon name="accessibility-outline"></ion-icon>
+            </span>
+            <span className="bottomnav__text">BMI</span>
+          </CustomLink>
+
+          <CustomLink to="/gymsrp-react/contact" className="bottomnav__link">
+            <span className="bottomnav__icon">
+              <ion-icon name="mail-outline"></ion-icon>
+            </span>
+            <span className="bottomnav__text">Contact</span>
+          </CustomLink>
+
           <div className="bottomnav__indicator"></div>
         </ul>
       </div>
     </div>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+  return (
+    <li className={isActive ? "bottomnav__list active" : "bottomnav__list"}>
+      <NavLink to={to} {...props}>
+        {children}
+      </NavLink>
+    </li>
   );
 }
 
